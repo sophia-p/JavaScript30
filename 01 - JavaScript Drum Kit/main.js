938705f7004f8d313ad5756c9ffac34034e35bd9
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-17 14:37:42 -0400

    end CSS animation

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,4 +1,7 @@
-window.addEventListener('keydown', function(event){
+
+
+function playSound(event) {
+	
 	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`); //for animation 
 	// backticks for template literals to allow for interpolating variables
@@ -6,4 +9,14 @@ window.addEventListener('keydown', function(event){
 	audio.currentTime = 0; //rewind to the start to sounds can be played in succession
 	audio.play();
 	key.classList.add('playing'); //add playing class to div el
-})
+}
+
+function removeTransition(event){
+	if(event.propertyName !== 'transform') return; //skip it if it's not a transform
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key'); //node list of corresponding elements
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
