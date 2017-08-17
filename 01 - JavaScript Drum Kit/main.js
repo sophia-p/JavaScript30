1e44fe95d4fb1634b6918f88044712e72fb3e3cd
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-17 11:17:49 -0400

    select corresponding div

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,6 +1,9 @@
 window.addEventListener('keydown', function(event){
 	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`); //for animation 
 	// backticks for template literals to allow for interpolating variables
 	if(!audio) return; //stop the function from running if key pressed doesn't have sound
+	audio.currentTime = 0; //rewind to the start to sounds can be played in succession
 	audio.play();
+	console.log(key);
 })
