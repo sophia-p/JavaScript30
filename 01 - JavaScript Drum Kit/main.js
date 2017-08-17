d2d3e222b15db895852e57db277d915e6ba3f6ca
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-17 11:14:42 -0400

    test play

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('keydown', function(event){
 	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 	// backticks for template literals to allow for interpolating variables
-	console.log(audio);
+	if(!audio) return; //stop the function from running if key pressed doesn't have sound
+	audio.play();
 })
