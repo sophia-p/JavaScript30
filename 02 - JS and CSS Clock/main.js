9ebd4557832f5e5cb6befeb698712da4930c094b
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-22 18:53:24 -0400

    add min and hour movements

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,11 +1,21 @@
-const secondHand = document.querySelector(".second-hand")
-
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 function setDate() {
 	const now = new Date();
 	const seconds = now.getSeconds();
-	const secondsDegrees = (seconds/60) * 360; //percentage * 360
+	const secondsDegrees = ((seconds/60) * 360) + 90; //percentage * 360, plus 90 to offset default 90deg
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-	console.log(seconds);
+	//minutes
+	const minutes = now.getMinutes();
+	const minutesDegrees = ((minutes/60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	//hours
+	const hours = now.getHours();
+	const hoursDegrees = ((hours/12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+	console.log(hours);
+
 }
 
 setInterval(setDate, 1000);
