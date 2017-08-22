1b42dc1b4cc1a7632db4e859e72fa875e2de90ef
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-22 18:42:03 -0400

    second hand ticks

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,5 +1,11 @@
+const secondHand = document.querySelector(".second-hand")
+
 function setDate() {
-	console.log('Hi')
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondsDegrees = (seconds/60) * 360; //percentage * 360
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	console.log(seconds);
 }
 
 setInterval(setDate, 1000);
