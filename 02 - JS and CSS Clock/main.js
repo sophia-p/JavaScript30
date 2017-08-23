8588c94e8fd94c5f8c27b56d32a7049fe4e502c4
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-22 21:26:26 -0400

    a bit jumpy

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -14,7 +14,7 @@ function setDate() {
 	const hours = now.getHours();
 	const hoursDegrees = ((hours/12) * 360) + 90;
 	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-	console.log(hours);
+
 
 }
 
