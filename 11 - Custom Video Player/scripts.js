7c72844b03393022d888b4645b9a0c95ed1746f3
Sophia Pignatelli <pignatelli@fordham.edu>
2017-09-08 17:46:41 -0400

    update volume and speed ranges

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -28,6 +28,10 @@ function skip(){
 	video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate(){
+	video[this.name] = this.value;
+}
+
 //hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -36,5 +40,8 @@ video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 
